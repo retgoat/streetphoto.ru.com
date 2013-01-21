4467a616bb8b2ae4e62886da30dcc2bc3480b916
Roman Sotnikov <roman.sotnikov@gmail.com>
2013-01-21 15:56:35 +0700

    Remove news section from translations RU

--- views/index.rb
+++ views/index.rb
@@ -6,21 +6,9 @@ class App
     	end
 
     	def newsblock_header
-    		@t.news.newsblock_header
+    		@t.newsblock_header
     	end
-
-    	# def news_1_header
-    	# 	@t.news.n1.header
-    	# end
-
-    	# def news_1_date
-    	# 	@t.news.n1.date
-    	# end
-
-    	# def news_1_body
-    	# 	@t.news.n1.body
-    	# end
-  	  def news_out
+        def news_out
 		    news_file = YAML::load_file('./news/news.yaml')
   		end
     end
